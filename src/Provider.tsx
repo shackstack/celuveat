fa5db923218c765c7684f61c7863b0c04aefe35e
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-01-12 23:00:32 +0900

    chore(#5): set Provider

---
+++ src/Provider.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+const queryClient = new QueryClient({});
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Provider;
