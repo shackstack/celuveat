89cf864834532ea929dd1bb6271b6b0d0fbc404e
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-01-18 23:40:53 +0900

    chore(#7): vite svg 사용환경구축

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
     react({
       jsxImportSource: "@emotion/react",
     }),
+    svgr(),
   ],
 });
