6beff3d12c51b10e03354345cf6ea8fa3d508dc4
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-01-19 19:43:50 +0900

    feat(#21): 리뷰 더보기 페이지

--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import ReviewCard from "@/components/ReviewCard";
 import { useUserProfileQuery } from "@/hooks/server/members";
 import { useRestaurantReviewsQuery } from "@/hooks/server/reviews";
@@ -11,21 +12,25 @@ const ReviewsPage = () => {
   );
   const { data: myProfile } = useUserProfileQuery();
 
+  // 제목고도화 더 필요
   return (
-    <ul className="mt-16 flex flex-col px-20">
-      {reviews.pages.map((page) =>
-        page.contents.map((review) => (
-          <>
-            <ReviewCard
-              key={review.id}
-              review={review}
-              isMyReview={myProfile!.id === review.writer.id}
-            />
-            <hr className="my-16 h-1 w-full bg-gray-100" />
-          </>
-        ))
-      )}
-    </ul>
+    <>
+      <Header withBack title="리뷰 더보기" />
+      <ul className="mt-16 flex flex-col px-20">
+        {reviews.pages.map((page) =>
+          page.contents.map((review) => (
+            <>
+              <ReviewCard
+                key={review.id}
+                review={review}
+                isMyReview={myProfile!.id === review.writer.id}
+              />
+              <hr className="my-16 h-1 w-full bg-gray-100" />
+            </>
+          ))
+        )}
+      </ul>
+    </>
   );
 };
 
