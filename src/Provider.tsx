e711796015fa47d8f2b57bea14daba17da05b1d0
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-01-19 20:17:20 +0900

    feat(#23): lottie 적용

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,13 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { OverlayProvider } from "overlay-kit";
 import { ReactNode, Suspense } from "react";
+
+import { Lottie } from "@toss/lottie";
 
 const queryClient = new QueryClient({});
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="w-full h-[100vh] flex justify-center items-center">
+            <Lottie src="/public/spinner.json" width={120} height={120} />
+          </div>
+        }
+      >
         <OverlayProvider>{children}</OverlayProvider>
       </Suspense>
     </QueryClientProvider>
